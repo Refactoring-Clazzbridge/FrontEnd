cb734308b8856524b49d0c2105e47c0052a13bc7
Seulki Kang <skbb11@naver.com>
2024-10-31 14:46:35 +0900

    fix:배포

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 const socket = io(
-  "http://localhost:3001"
+  "http://default-websocket-servic-3b8f6-100169772-9abcce8b6147.kr.lb.naverncp.com:3001"
 );
 export default socket;
